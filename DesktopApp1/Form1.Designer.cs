18abc19429bbe6677de5f9533043c7a2dd1beb0c
LightningMods <34066913+LightningMods@users.noreply.github.com>
2019-06-07 19:21:11 -0400

    Add files via upload

---
+++ DesktopApp1/Form1.Designer.cs
@@ -0,0 +1,250 @@
+namespace DesktopApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.helloWorldLabel = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.progressBar3 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // helloWorldLabel
+            // 
+            this.helloWorldLabel.AutoSize = true;
+            this.helloWorldLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.helloWorldLabel.Location = new System.Drawing.Point(159, 24);
+            this.helloWorldLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.helloWorldLabel.Name = "helloWorldLabel";
+            this.helloWorldLabel.Size = new System.Drawing.Size(227, 26);
+            this.helloWorldLabel.TabIndex = 3;
+            this.helloWorldLabel.Text = "PS4 Package Installer";
+            this.helloWorldLabel.Click += new System.EventHandler(this.helloWorldLabel_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.FileName = "progressBar3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(87, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP Address";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(65, 128);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(93, 23);
+            this.metroButton2.TabIndex = 7;
+            this.metroButton2.Text = "Open Package";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(141, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(164, 82);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.PromptText = "192.168.1.3";
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(163, 23);
+            this.metroTextBox1.TabIndex = 8;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMark = "192.168.1.3";
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(141, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.Lines = new string[0];
+            this.metroTextBox2.Location = new System.Drawing.Point(164, 127);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(163, 23);
+            this.metroTextBox2.TabIndex = 9;
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(333, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 32);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(148, 229);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(148, 40);
+            this.metroButton1.TabIndex = 11;
+            this.metroButton1.Text = "Install Package";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(5, 272);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Copyright DarkSoftware LLC ©";
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(145, 157);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(47, 13);
+            this.lblProgress.TabIndex = 14;
+            this.lblProgress.Text = "Connect";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(145, 197);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(29, 13);
+            this.label15.TabIndex = 15;
+            this.label15.Text = "Prog";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(148, 173);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(179, 21);
+            this.progressBar1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.helloWorldLabel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label helloWorldLabel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.OpenFileDialog progressBar3;
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private System.Windows.Forms.Button button1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ DesktopApp1/Form1.cs
@@ -0,0 +1,342 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+using System.Net;
+using System.Runtime.CompilerServices;
+using MetroFramework;
+using Rebex.IO;
+using System.IO.Compression;
+using Rebex.Net;
+using Microsoft.Win32;
+
+/*Copyright Darksoftware (c) 2019*/
+
+namespace DesktopApp1
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            metroTextBox1.Text = Properties.Settings.Default.IP;
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+ 
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void helloWorldLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+  
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog opendialog = new OpenFileDialog();
+            opendialog.CheckFileExists = true;
+            //opendialog.AddExtension 
+            opendialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            opendialog.Multiselect = false;
+            opendialog.Filter = "PS4 Package File (*.pkg) | *.pkg";
+            if (opendialog.ShowDialog() == DialogResult.OK)
+            {
+                string FilePath = opendialog.FileName;
+                metroTextBox2.Text = FilePath;
+ 
+            }
+
+        }
+
+
+
+        private bool _isWorking;                    // determines whether any operation is running
+        private DateTime _lastTransferProgressTime;      // last TransferProgress event call time
+        private DateTime _transferTime;             // transfer launch-time
+        private bool _cccMode = false;
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            // create an FTP clien
+  
+
+        }
+
+        private void TransferCompleted()
+        {
+            _isWorking = false;
+            MessageBox.Show("Finished");
+            SetProgressValue(0);
+        }
+
+        /// <summary>
+        /// show transfer status: files, bytes, time, speed
+        /// </summary>
+        private void ShowTransferStatus(long bytesTransferred, int filesTransferred)
+        {
+            // unknown bytes transferred
+
+
+            if (bytesTransferred == 0)
+                return;
+
+            // files and bytes transferred
+            string outstring = string.Format("{0} file{1} ({2} byte{3}) transferred in",
+                filesTransferred, (filesTransferred > 1 ? "s" : null),
+                bytesTransferred, (bytesTransferred > 1 ? "s" : null));
+
+            // time spent
+            TimeSpan ts = DateTime.Now - _transferTime;
+
+            // speed
+            if (ts.TotalSeconds > 1)
+            {
+                outstring += (ts.Days > 0 ? " " + ts.Days + " day" + (ts.Days > 1 ? "s" : null) : null);
+                outstring += (ts.Hours > 0 ? " " + ts.Hours + " hour" + (ts.Hours > 1 ? "s" : null) : null);
+                outstring += (ts.Minutes > 0 ? " " + ts.Minutes + " min" + (ts.Minutes > 1 ? "s" : null) : null);
+                outstring += (ts.Seconds > 0 ? " " + ts.Seconds + " sec" + (ts.Seconds > 1 ? "s" : null) : null);
+            }
+            else
+            {
+                outstring += " " + ts.TotalSeconds + " sec";
+            }
+
+            double speed = bytesTransferred / ts.TotalSeconds;
+            if (speed < 1)
+                outstring += string.Format(" at {0:F3} B/s", speed);
+            else if (speed < 1024)
+                outstring += string.Format(" at {0:F0} B/s", speed);
+            else
+                outstring += string.Format(" at {0:F0} KB/s", speed / 1024);
+
+        }
+
+        void Traversing(object sender, FtpTraversingEventArgs e)
+        {
+            if (e.Action == TransferAction.Listing)
+                return;
+
+            string strBatchInfo = string.Format("({0} file{1} traversed)    ",
+                e.FilesTotal, (e.FilesTotal > 1 ? "s" : null));
+
+            switch (e.TraversingState)
+            {
+                case TraversingState.DirectoryRetrieved:
+                    strBatchInfo += "Directory retrieved.";
+                    break;
+                case TraversingState.DirectoryRetrieving:
+                    strBatchInfo += "Retrieving directory...";
+                    break;
+                case TraversingState.HierarchyRetrieved:
+                    strBatchInfo += string.Format("Hierarchy retrieved ({0} byte{1} in {2} file{3}).",
+                        e.BytesTotal, (e.BytesTotal > 1 ? "s" : null),
+                        e.FilesTotal, (e.FilesTotal > 1 ? "s" : null));
+                    break;
+                case TraversingState.HierarchyRetrieving:
+                    strBatchInfo += "Retrieving hierarchy...";
+                    break;
+            }
+
+            SetText(strBatchInfo);
+        }
+
+        /// <summary>
+        /// handles the transfer progress changed event
+        /// </summary>
+        void TransferProgressChanged(object sender, FtpTransferProgressChangedEventArgs e)
+        {
+            string strBatchInfo = string.Format("({0} / {1} file{2} processed)    ",
+                e.FilesProcessed, e.FilesTotal, (e.FilesProcessed > 1 ? "s" : null));
+
+            SetProgressValue(Convert.ToInt32(e.ProgressPercentage));
+
+            switch (e.TransferState)
+            {
+                case TransferProgressState.DataBlockProcessed:
+                    strBatchInfo += e.BytesTransferred + " bytes";
+                    break;
+                case TransferProgressState.DirectoryProcessing:
+                    strBatchInfo += "Processing directory...";
+                    break;
+                case TransferProgressState.FileTransferring:
+                    strBatchInfo += "Transferring file...";
+                    break;
+                case TransferProgressState.FileTransferred:
+                    strBatchInfo += "File transferred.";
+                    break;
+                case TransferProgressState.TransferCompleted:
+                    strBatchInfo += "Transfer completed.";
+                    ShowTransferStatus(e.BytesTransferred, e.FilesTransferred);
+                    break;
+            }
+
+            SetText(strBatchInfo);
+        }
+
+
+        private void SetProgressValue(int value)
+        {
+            // workaround for progress bar smoothing
+
+            Invoke(new Action(() => {
+                if (value == 100)
+            {
+                progressBar1.Maximum = 101;
+                progressBar1.Value = 101;
+                progressBar1.Maximum = 100;
+                System.Threading.Thread.Sleep(200);
+            }
+            else
+            {
+                progressBar1.Value = value;
+            }
+            }));
+        }
+
+        private void SetText(string text)
+        {
+            Invoke(new Action(() => { lblProgress.Text = text; }));
+        }
+
+        private void SetTex(string text)
+        {
+            Invoke(new Action(() => { label15.Text = text; }));
+        }
+
+        private void StateChanged(object sender, FtpStateChangedEventArgs e)
+        {
+            switch (e.NewState)
+            {
+                case FtpState.Disconnected:
+                case FtpState.Disposed:
+                    SetTex("Disconnected");
+                    break;
+                case FtpState.Ready:
+                    SetTex("Ready");
+                    break;
+            }
+        }
+
+        private void DeleteProgressChanged(object sender, FtpDeleteProgressChangedEventArgs e)
+        {
+            string strDeleteInfo = string.Format("({0} / {1} file{2} deleted)    ",
+                e.FilesProcessed, e.FilesTotal, (e.FilesProcessed > 1 ? "s" : null));
+
+            switch (e.DeleteState)
+            {
+                case DeleteProgressState.DeleteCompleted:
+                    strDeleteInfo += "Delete completed.";
+                    break;
+                case DeleteProgressState.DirectoryDeleted:
+                    strDeleteInfo += "Directory deleted.";
+                    break;
+                case DeleteProgressState.DirectoryProcessing:
+                    strDeleteInfo += "Processing directory...";
+                    break;
+                case DeleteProgressState.FileDeleted:
+                    strDeleteInfo += "File deleted.";
+                    break;
+                case DeleteProgressState.FileDeleting:
+                    strDeleteInfo += "Deleting file...";
+                    break;
+            }
+
+            SetText(strDeleteInfo);
+        }
+
+        private void ProblemDetected(object sender, FtpProblemDetectedEventArgs e)
+        {
+            // MessageBox.Show("Problem Detected " + e);
+        }
+
+        private void god(string path, string dir)
+        {
+            using (Ftp client = new Ftp())
+            {
+
+                try
+                {
+                    // connect and login to the FTP
+                    client.Connect(path);
+                    client.Login("anonymous", "DONT-LOOK@MYCODE");
+
+                    client.StateChanged += StateChanged;
+                    client.Traversing += Traversing;
+                    client.TransferProgressChanged += TransferProgressChanged;
+                    client.DeleteProgressChanged += DeleteProgressChanged;
+                    client.ProblemDetected += ProblemDetected;
+
+                   client.PutFile(dir, @"/user/app/temp.pkg");
+
+                    client.SendCommand("installpkg");
+
+                    var response = client.ReadResponse();
+
+                    MessageBox.Show(response.Raw);
+
+                    SetText("Package Sent");
+
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                client.Disconnect();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.IP = metroTextBox1.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Started");
+            Task.Run(() => god(metroTextBox1.Text, metroTextBox2.Text));
+        }
+
+
+    }
+}
